59aa48bd96a8bf6c8e031bc30dff711aaae1e71a
Vignesh <vigneshsweg123@gmail.com>
2021-11-05 17:25:48 +0530

    Deploy

---
+++ index.js
@@ -0,0 +1,42 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+const { METHODS } = require("http");
+const { Socket } = require("socket.io");
+
+// SOCKET IO
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors());
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send("Server is Running");
+});
+
+//BACKEND CALL CONNECTION USING SOCKETS
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callEnded');
+    });
+
+    socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit('callUser', { signal: signalData, from, name });
+    });
+
+    socket.on('answerCall', (data) => {
+        io.to(data.to).emit('callAccepted', data.signal);
+    });
+
+});
+
+server.listen(PORT, () => console.log('Server is in PORT ' + `${PORT}`));
